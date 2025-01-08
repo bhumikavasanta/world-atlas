c17f3b066715e47d96fbf878458858087c2aaf04
Bhumika Vasanta <bhumikavasanta2460@gmail.com>
2025-01-08 17:04:33 +0530

    Footer Added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import footerContact from '../API/footer.json';
+import { MdPlace } from 'react-icons/md';
+import { IoCallSharp } from 'react-icons/io5';
+import { TbMailPlus } from 'react-icons/tb';
 
 const Footer = () => {
+
+    const footerIcon = {
+        MdPlace: <MdPlace />,
+        IoCallSharp: <IoCallSharp />,
+        TbMailPlus: <TbMailPlus />,
+    }
+
     return (
-        <h1>
-            Footer
-        </h1>
+        <footer className="footer-section">
+            <div className="container grid grid-three-cols">
+                {footerContact.map((curData, index) => {
+                    const { icon, title, details } = curData;
+                    return (
+                        <div className="footer-contact" key={index}>
+                            <div className="icon">{footerIcon[icon]}</div>
+                            <div className="footer-contact-text">
+                                <p>{title}</p>
+                                <p>{details}</p>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+
+            <div className="copyright-area">
+                <div className="container">
+                    <div className="grid grid-two-cols">
+                        <div className="copyright-text">
+                            <p>
+                                Copyright &copy; 2025
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
     );
 }
 
